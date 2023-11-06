1693f06a08ecf52f8d9fa0b7b5b06ade79894e81
niblicat <maybenaebli@gmail.com>
2023-11-05 23:02:52 -0600

    cleaned up comments and imports

--- src/routes/autocomplete.ts
+++ src/routes/autocomplete.ts
@@ -1,12 +1,6 @@
 import { writable, type Writable } from "svelte/store";
-import * as readline from 'readline';
-import * as fs from 'fs';
 import Dictionary from '$lib/dictionary.txt?raw';
 
-
-// const compares: string[] = [];
-
-// var text = fs.readFileSync('dictionary.txt').toString('utf-8');
 const compares: string[] = Dictionary.split('\n');
 
 export var related: Writable<string[]> = writable([]);

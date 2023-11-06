9a78da63d6ef8287adaf5a32fa324f0e1bd033ba
niblicat <maybenaebli@gmail.com>
2023-11-05 23:00:18 -0600

    dictionary now imported into str array

--- src/routes/autocomplete.ts
+++ src/routes/autocomplete.ts
@@ -1,9 +1,19 @@
 import { writable, type Writable } from "svelte/store";
+import * as readline from 'readline';
+import * as fs from 'fs';
+import Dictionary from '$lib/dictionary.txt?raw';
 
-export let related: Writable<string[]> = writable([]);
+
+// const compares: string[] = [];
+
+// var text = fs.readFileSync('dictionary.txt').toString('utf-8');
+const compares: string[] = Dictionary.split('\n');
+
+export var related: Writable<string[]> = writable([]);
 
 export function PopulateWords(word: string) {
     related.set([
         word
     ]);
+    related.set(compares);
 }
